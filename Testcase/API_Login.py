371ae2660bc25e09747709a545a2066fe31e115a
anntt061298 <annguyhust1998@gmail.com>
2024-09-14 16:16:36 +0700

    Write testcase API Login

---
+++ Testcase/API_Login.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python3
+
+TEST_SUITE_NAME = "Sample Test API"
+
+import unittest
+# fast_api_root.py
+import sys
+import os
+import time
+import copy
+
+
+
+# Thêm thư mục cha của test_utils vào sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Bây giờ bạn có thể nhập test_utils
+from test_utils import *
+
+BASE_URL = f"https://wts.finavi.com.vn/api/v1/"
+
+headers = {
+        "Content-Type": "application/json; charset=UTF-8",        
+    }
+USERNAME = "An061298"
+PASSWORD = "ThuAn061298"
+
+PAYLOAD = {
+    "grant_type": "password",
+    "client_id": "grooo",
+    "client_secret": "grooo",
+    "platform": "WEB",
+    "username": USERNAME,
+    "password": PASSWORD
+}
+
+class CurlTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+        # refresh_token = read_refresh_token_config()
+        # time.sleep(2)
+        # cls.access_token = refresh_access_token(refresh_token)
+
+    def setUp(self):
+    # This method will be run before each test case
+        # self.payload = dict(PAYLOAD)  # Khôi phục bản sao của PAYLOAD
+        self.payload = copy.deepcopy(PAYLOAD)  # Tạo bản sao của PAYLOAD
+
+    def test01(self):
+        """ Login.01: Login successfull """
+        url = BASE_URL + "login"
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 200)
+        # print(response_data)
+        self.assertEqual(response_data["accountList"][0]["id"], 10)
+        self.assertEqual(response_data["accountList"][0]["userId"], 8)
+        self.assertEqual(response_data["accountList"][0]["nickName"], "NM")
+        self.assertEqual(response_data["accountList"][0]["accountNumber"], "050C158527")
+        self.assertEqual(response_data["accountList"][0]["subNumber"], "0001158527NM")
+        self.assertEqual(response_data["accountList"][0]["cusType"], "I")
+
+    def test02(self):
+        """ Login.02: Login fail : Missing field grant_type """
+        url = BASE_URL + "login"
+
+        self.payload["grant_type"] = ""
+        print(PAYLOAD)
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test03(self):
+        """ Login.03: Login fail : Delete field grant_type """
+        url = BASE_URL + "login"
+
+        del self.payload["grant_type"] 
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test04(self):
+        """ Login.04: Login fail : Missing enter client_id  """
+        url = BASE_URL + "login"
+
+        self.payload["client_id"] = ""
+        print(self.payload)
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test05(self):
+        """ Login.05: Login fail : Delete field client_id  """
+        url = BASE_URL + "login"
+
+        del self.payload["client_id"] 
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test06(self):
+        """ Login.06: Login fail : Missing enter field client_secret  """
+        url = BASE_URL + "login"
+
+        self.payload["client_secret"] = ""
+    
+        # Send POST PAYLOAD
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test07(self):
+        """ Login.07: Login fail : Delete field client_secret  """
+        url = BASE_URL + "login"
+
+        del self.payload["client_secret"]
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test08(self):
+        """ Login.08: Login fail : Missing enter field platform  """
+        url = BASE_URL + "login"
+
+        self.payload["platform"] =""
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+    def test09(self):
+        """ Login.09: Login fail : Delete field platform  """
+        url = BASE_URL + "login"
+
+        del self.payload["platform"]
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+        # print (response_data)
+
+
+    def test10(self):
+        """ Login.10: Login fail : Missing enter field Username """
+        url = BASE_URL + "login"
+
+        self.payload["username"] = ""
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+
+    def test11(self):
+        """ Login.11: Login fail : Delete field Username """
+        url = BASE_URL + "login"
+
+        del self.payload["username"]
+    
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 422)
+
+    def test12(self):
+        """ Login.12: Login fail : Enter username dont exist """
+        url = BASE_URL + "login"
+
+#         PAYLOAD = {
+#                     "grant_type": "password",
+#                     "client_id": "grooo",
+#                     "client_secret": "grooo",
+#                     "platform": "WEB",
+#                     "username": "USLorem Ipsum is simply dummy text of the printing and typesetting industry.ERNAME",
+#                     "password": PASSWORD
+# }
+        self.payload["username"] = "USLorem Ipsum is simply dummy text of the printing and typesetting industry.ERNAME"
+        # print(self.payload)
+
+        # Send POST request
+        status_code, response_data = send_request(
+            url=url, method="POST", headers=headers, data = self.payload
+        )
+
+        # Validate HTTP response code, fail if not 200
+        self.assertEqual(status_code, 400)
+        # print(response_data)
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    print("Running Test Suite")
+
+    script = ArgumentParser(description=TEST_SUITE_NAME, epilog="Note: any note here")
+    # script.add_argument("Config", type=str, help="Config file")
+    args, unittest_args = script.parse_known_args()
+
+    from test_utils import testcase_sort_func
+
+    unittest.defaultTestLoader.sortTestMethodsUsing = testcase_sort_func
+
+    from framework import CustomTestRunner
+
+    unittest.main(argv=[__file__] + unittest_args, testRunner=CustomTestRunner)
--- test_utils.py
+++ test_utils.py
@@ -21,121 +21,33 @@ def testcase_sort_func(a,b):
 #     config.read(config_file)
 #     return config
 
-def login_success():
+# def login_success():
     
-    url = "https://dev2-retail-api.equix.app/v1/auth"
+#     url = "https://wts.finavi.com.vn/api/v1/"
 
-    # Headers
-    headers = {
-        "Content-Type": "application/json",
-        "environment": "equix",
-    }
+#     # Headers
+#     headers = {
+#         "Content-Type": "application/json; charset=UTF-8",        
+#     }
 
-    data = {
-    "data": {
-        "username": "thu-an.nguyen@novus-fintech.com",
-        "password": "Ab@12345",
-        "provider": "paritech",
-        "storage_token": True
-    }
-}
+#     data = {
+#     "grant_type": "password",
+#     "client_id": "grooo",
+#     "client_secret": "grooo",
+#     "platform": "WEB",
+#     "username": "An061298",
+#     "password": "ThuAn061298"
+# }
 
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        refresh_token = data.get("refreshToken")
-        return refresh_token
-    else:
-        print(f"Error refreshing access token: {response.status_code}")
-        print(response.text)
-        return None
-    
-def decode(refresh_token):
-    url = "https://dev2-retail-api.equix.app/v1/auth/decode"
-
-    # Headers
-    headers = {
-        "Content-Type": "application/json",
-        "environment": "equix",
-    }
-
-    data = {
-    "data": {
-        "token": refresh_token,
-        "pin": "111111"
-    }
-}
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        token = data.get("token")
-        return token
-    else:
-        print(f"Error refreshing access token: {response.status_code}")
-        print(response.text)
-        return None    
-
-def read_refresh_token_config(file_name='config1.ini'):
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = current_path+ "/" + file_name
-
-    config = configparser.ConfigParser()
-    config.read(file_path)
-
-    section = 'DEFAULT'
-    key = 'refresh-token'
-
-    refresh_token = config.get(section, key)
-    return refresh_token
-
-def refresh_access_token(token):
-    url = "https://dev2-retail-api.equix.app/v1/auth/refresh"
-
-    # Headers
-    headers = {
-        "Content-Type": "application/json",
-        "environment": "tradeforgood",
-        "origin": "https://dev2.equix.app"
-
-    }
-
-    data = {
-        "data": {
-            "refreshToken": token,
-            "deviceID": "4216cf3e-108a-4738-958b-3420f3523557",
-        }
-    }
-
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        new_access_token = data.get("accessToken")
-        return new_access_token
-    else:
-        print(f"Error refreshing access token: {response.status_code}")
-        print(response.text)
-        return None
-    
-
-# try:
-#     refresh_token = login_success()
-#     if refresh_token:
-#         print('Refresh Token:', refresh_token)
-        
-#         token = decode(refresh_token)
-#         if token:
-#             print('Token:', token)
-#             new_access_token = refresh_access_token(token)
-#             if new_access_token:
-#                 print('new_access_token:', new_access_token)
-#             else:
-#                 print('Failed to get new access token.')
-#         else:
-#             print('Failed to get token.')
+#     response = requests.post(url, json=data, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         refresh_token = data.get("refreshToken")
+#         return refresh_token
 #     else:
-#         print('Failed to get refresh token.')
-# except Exception as e:
-#     print(str(e))
+#         print(f"Error refreshing access token: {response.status_code}")
+#         print(response.text)
+#         return None
 
 
 def send_request(url, method, headers=None, data=None):
